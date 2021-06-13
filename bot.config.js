647bc48492c58838a12ee5f83064d67c6385425e
a22056988 <39297292+a22056988@users.noreply.github.com>
2021-06-13 14:26:18 +0800

    Update bot.config.js

--- bot.config.js
+++ bot.config.js
@@ -1,9 +1,9 @@
 const linebot = require('linebot');
 
 var bot = linebot({
-    channelId: '請輸入',
-    channelSecret: '請輸入',
-    channelAccessToken: '請輸入',
+    channelId:U96bddabd5d34be7c038f96f25fc74466,
+    channelSecret:684ff8eafe8d76f132d8a934df60e735,
+    channelAccessToken:yrVoQ0yRwWvlg0Q5HwZvAMylg5HIz3vzHQLeoqGVgZPPFeKqncqkWgXbOH2qosmXigk2z+KrGghILIIgIUHOg/dL67Ge9qHKkGlqwWn4sc6AskcrAMWU/LI8lSQE9MB7OerPT4yIL6EL2E7HJVG1iQdB04t89/1O/w1cDnyilFU=,
 })
 
 module.exports = bot
